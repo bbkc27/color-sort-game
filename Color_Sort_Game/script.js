01b34987b556f163e18ec75598717f21aa5e53ad
bbkc27 <briana.j.casey@gmail.com>
2022-03-08 20:26:37 -0500

    modal functionality

--- Color_Sort_Game/script.js
+++ Color_Sort_Game/script.js
@@ -1,1 +1,18 @@
 
+//BUTTONS - GRABBING ELEMENTS
+let howToPlayButton = document.querySelector(".howToPlayButton");
+let modal = document.querySelector(".modal");
+let closeModalButton = document.querySelector(".closeModalButton");
+
+//BUTTONS - FUNCTIONS
+const openHowToPlayModal = () => {
+    modal.style.display = "block";
+}
+
+const closeHowToPlayModal = () => {
+    modal.style.display = "none";
+}
+
+//BUTTONS - EVENT LISTENERS
+howToPlayButton.addEventListener("click", openHowToPlayModal);
+closeModalButton.addEventListener("click", closeHowToPlayModal);
