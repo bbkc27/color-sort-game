f414df1f53ab6e01411e6390c6ee604d8a7ce965
bbkc27 <briana.j.casey@gmail.com>
2022-03-09 20:31:02 -0500

    top ball in tube draggable and droppable in tubes with less than 3 balls

--- Color_Sort_Game/script.js
+++ Color_Sort_Game/script.js
@@ -3,6 +3,11 @@
 let howToPlayButton = document.querySelector(".howToPlayButton");
 let modal = document.querySelector(".modal");
 let closeModalButton = document.querySelector(".closeModalButton");
+let resetButton = document.getElementById("resetButton");
+
+//COLORS - GRABBING ELEMENTS
+let boxes = document.querySelectorAll(".box");
+let ball = document.querySelectorAll(".ball");
 
 //BUTTONS - FUNCTIONS
 const openHowToPlayModal = () => {
@@ -13,24 +18,38 @@ const closeHowToPlayModal = () => {
     modal.style.display = "none";
 }
 
+const resetGame = () => { 
+    location.reload();
+}
 
-//BALLS - FUNCTIONS
+//COLORS - FUNCTIONS
 
 const allowDrop = (ev) => {
     ev.preventDefault();
 }
 
-const drag = (ev) => {
+const drag = (ev) => {   
+    if(ev.target === ev.target.parentElement.firstElementChild){
     ev.dataTransfer.setData("text", ev.target.id);
+    } else {
+        ev.preventDefault();
+    }
 }
 
 const drop = (ev) => {
-    let data = ev.dataTransfer.getData("text");
-    ev.target.textContent = data;
+    if (ev.target.children.length < 3){
     ev.preventDefault();
+
+
+    console.log()    
+
+    let data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+    } 
 }
 
 //BUTTONS - EVENT LISTENERS
 howToPlayButton.addEventListener("click", openHowToPlayModal);
 closeModalButton.addEventListener("click", closeHowToPlayModal);
+resetButton.addEventListener("click", resetGame);
 
