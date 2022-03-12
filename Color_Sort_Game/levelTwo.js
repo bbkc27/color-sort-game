b17fbe663e40357da6ed2e9d060fef7d2ddaa4c7
bbkc27 <briana.j.casey@gmail.com>
2022-03-12 15:05:01 -0500

    Level Two

--- Color_Sort_Game/levelTwo.js
+++ Color_Sort_Game/levelTwo.js
@@ -4,6 +4,7 @@ let howToPlayButton = document.querySelector(".howToPlayButton");
 let modal = document.querySelector(".modal");
 let closeModalButton = document.querySelector(".closeModalButton");
 let resetLevelButton = document.getElementById("resetLevelButton");
+let resetGameButton = document.getElementById("resetGameButton");
 
 //COLORS - GRABBING ELEMENTS
 let boxes = document.querySelectorAll(".box");
@@ -13,7 +14,7 @@ let ball = document.querySelectorAll(".ball");
 let wins = 0;
 
 //WINNING FUNCTION
-const completeLevelOne = () => {
+const completeLevelTwo = () => {
     document.body.classList.add('game-over')
 }
 
@@ -29,6 +30,7 @@ const closeHowToPlayModal = () => {
 const resetLevel = () => { 
     location.reload();
 }
+
 
 
 //COLORS - FUNCTIONS
@@ -53,7 +55,7 @@ const drag = (ev) => {
 
 
 const drop = (ev) => {
-    if (ev.target.children.length >= 3 || ev.target.classList[0] === "ball"){
+    if (ev.target.children.length >= 4 || ev.target.classList[0] === "ball"){
         console.log("Cannot Drop Ball Here.")
 
     } else if (ev.target.children.length === 0){
@@ -78,12 +80,19 @@ const drop = (ev) => {
         let data = ev.dataTransfer.getData("text");
         ev.target.appendChild(document.getElementById(data));
 
+    } else if (ev.target.children.length == 3 && ev.target.lastElementChild.classList[1] === draggedBall.classList[1]){
+        ev.preventDefault(); 
+
+        let data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+
         for (let i=0; i<6; i++){
-            if(boxes[i].children.length === 3 && boxes[i].children[0].classList.value == boxes[i].children[1].classList.value && boxes[i].children[1].classList.value == boxes[i].children[2].classList.value){
+            if(boxes[i].children.length === 4 && boxes[i].children[0].classList.value == boxes[i].children[1].classList.value && boxes[i].children[1].classList.value == boxes[i].children[2].classList.value && boxes[i].children[2].classList.value == boxes[i].children[3].classList.value){
                 wins += 1
+                console.log(wins)
                 if (wins == 10){
                     console.log("You Won!")
-                    completeLevelOne();
+                    completeLevelTwo();
                 }
             }
         }
